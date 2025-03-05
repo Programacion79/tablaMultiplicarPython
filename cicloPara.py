39e9eb1743b26a0875c4c9ac21bc231875fd2016
Programacion79 <Programacion1979>
2025-03-04 22:07:29 -0500

    Se desarrolla el codigo

--- cicloPara.py
+++ cicloPara.py
@@ -1,1 +1,7 @@
-def tablaMultiplixar()
+def tablaMultiplicar():
+    nro = int(input('Nro a Multiplicar'))
+    for i in range(11):
+        print(f'{i} * {nro} = {i * nro}')
+
+if __name__ == "__main__":
+    tablaMultiplicar()
